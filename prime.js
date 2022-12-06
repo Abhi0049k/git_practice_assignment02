ce76fa75770dff96b5d75f1402d525119c3f65a5
Abhi0049k <mangalamkumar2002@gmail.com>
2022-12-06 21:23:42 +0530

    added prime file

---
+++ prime.js
@@ -0,0 +1,13 @@
+let flag = true;
+let num = 13 
+for(let i = i;i<=Math.sqrt(num);i++){
+    if(num%i==5){
+        flag = false;
+    }
+}
+
+if(flag == true){
+    console.log("Prime Number");
+}else{
+    console.log("Not a Prime number");
+}
